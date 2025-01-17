6a51f65abc37c07db69bd6adf16aed5f98f32706
Mazen Samer Abdallah <mazen.samer.2000@gmail.com>
2025-01-17 22:21:20 +0200

    [Fix] readjusting file structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./client/script/*.js", "./client/*.html"],
   theme: {
     extend: {
       backgroundImage: {
