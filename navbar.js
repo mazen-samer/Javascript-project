e2baf80aff5f498cb63d92194d4163e221e00f4a
Mazen Samer Abdallah <mazen.samer.2000@gmail.com>
2025-01-17 21:35:38 +0200

    [Feature] responsive navbar

--- navbar.js
+++ navbar.js
@@ -1,15 +1,47 @@
 let userName = sessionStorage.getItem("user");
+let userNameP = document.getElementById("username");
+let userNamePSm = document.getElementById("username-sm");
 let loginRegister = document.getElementById("login-register");
+let loginRegisterSm = document.getElementById("login-register-sm");
 let userNameLogout = document.getElementById("username-logout");
+let userNameLogoutSm = document.getElementById("username-logout-sm");
 let logout = document.getElementById("logout");
+let logoutSm = document.getElementById("logout-sm");
+
+let nav2 = document.getElementById("nav-2");
+let navOpenBtn = document.getElementById("nav-open-btn");
+let navCloseBtn = document.getElementById("nav-close-btn");
 
 if (userName) {
   userNameLogout.classList.add("flex");
   loginRegister.classList.add("hidden");
+  loginRegisterSm.classList.add("hidden");
+
+  userNameP.innerText = `Hello, ${userName}`;
+  userNamePSm.innerText = `Hello, ${userName}`;
 } else {
   userNameLogout.classList.add("hidden");
   loginRegister.classList.add("flex");
+  userNameLogoutSm.classList.add("hidden");
+  loginRegisterSm.classList.add("flex");
+
+  userNameP.innerText = `Hello`;
+  userNamePSm.innerText = `Hello`;
 }
+
+navOpenBtn.addEventListener("click", () => {
+  nav2.classList.remove("-right-full");
+  nav2.classList.add("right-0");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav2.classList.add("-right-full");
+  nav2.classList.remove("right-0");
+});
+
+nav2.addEventListener("click", () => {
+  nav2.classList.add("-right-full");
+  nav2.classList.remove("right-0");
+});
 
 logout.addEventListener("click", () => {
   sessionStorage.removeItem("user");
@@ -17,4 +49,16 @@ logout.addEventListener("click", () => {
   userNameLogout.classList.add("hidden");
   loginRegister.classList.remove("hidden");
   loginRegister.classList.add("flex");
+});
+logoutSm.addEventListener("click", () => {
+  sessionStorage.removeItem("user");
+  userNameLogout.classList.remove("flex");
+  userNameLogout.classList.add("hidden");
+  loginRegister.classList.remove("hidden");
+  loginRegister.classList.add("flex");
+
+  loginRegisterSm.classList.remove("hidden");
+  loginRegisterSm.classList.add("flex");
+  userNameLogoutSm.classList.remove("block");
+  userNameLogoutSm.classList.add("hidden");
 });
