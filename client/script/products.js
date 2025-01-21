b931cd218597f74fb7628d4d5615675a73c16d46
mazen-samer <mazen.samer.2000@gmail.com>
2025-01-22 00:52:00 +0200

    add card style

--- client/script/products.js
+++ client/script/products.js
@@ -34,24 +34,35 @@ function display() {
 
     // Generate HTML for each product
     for (var i = 0; i < data.length; i++) {
-            cols+= `
-            <div class="card max-w-sm bg-white border border-gray-200 rounded-lg shadow" data-id ="${data[i].id}">
+            cols+=`
+            <div class="flex flex-wrap justify-center w-200 gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-4 rounded-lg space-x-4">
+             <a href="#">
+       <div class="card m-7  w-96 h-[450px] sm:w-72 md:w-80 lg:w-80 rounded-xl shadow-lg overflow-hidden 
+         transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl" data-id="${data[i].id}">
+            <img class="p-8 rounded-t-lg w-full h-[60%] h-64 sm:h-72 md:h-80 lg:h-50 object-contain transition-transform duration-300 ease-in-out hover:scale-110" src="${data[i].image}" alt="${data[i].title}" />
+        </a>
+        <div class="px-5 pb-5">
             <a href="#">
-                <img class="p-2 w-24 h-24 mx-auto object-contain rounded-t-lg" src="${data[i].image}" alt="${data[i].title}" />
+                <h5 class="text-l font-semibold text-gray-900 dark:text-white line-clamp-1">${data[i].title}</h5>
             </a>
-            <div class="px-5 pb-5">
-                <a href="#">
-                    <h5 class="text-xl font-semibold tracking-tight text-gray-900">${data[i].title}</h5>
-                </a>
-                <div class="flex items-center mt-2.5 mb-5">
-                    <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded">Rating: 4.5</span>
-                </div>
-                <div class="flex items-center justify-between">
-                    <span class="text-3xl font-bold text-gray-900">$${data[i].price}</span>
-                    <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Add to cart</a>
+            <div class="flex items-center mt-2.5 mb-5">
+                <div class="flex items-center space-x-1 rtl:space-x-reverse">
+                 ${Array(5)
+                    .fill(
+                      '<svg class="w-5 h-5 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20"><path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/></svg>'
+                    )
+                    .join("")}
+
+
+                    <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
                 </div>
             </div>
-        </div> ` ;
+            <div class="flex items-center justify-between">
+                <span class="text-xl font-bold text-gray-900 dark:text-white">$${data[i].price}</span>
+            </div>
+        </div>
+    </div>
+</div>`;
     }
 
     // Insert generated HTML into the container
--- client/script/viewProduct.js
+++ client/script/viewProduct.js
@@ -27,21 +27,29 @@ function display(clickedProduct) {
 
   if (clickedProduct) {
     boxProduct.innerHTML = `
-        <div class="card max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-             <img class="p-2 w-24 h-24  mx-auto object-contain rounded-t-lg" src="${clickedProduct.image}" alt="${clickedProduct.title}" />
+    <div class="flex justify-center items-center">
+        <div class="card mt-20  w-96 h-[450px] sm:w-72 md:w-80 lg:w-80 rounded-xl shadow-lg overflow-hidden 
+        transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl">
+             <img class="p-2 w-60 h-40  mx-auto object-contain rounded-t-lg" src="${clickedProduct.image}" alt="${clickedProduct.title}" />
              <div class="px-5 pb-5">
              <h1 class="text-xl font-semibold tracking-tight text-gray-900">${clickedProduct.title}</h1>
              <div class="flex items-center mt-2.5 mb-5">
                 <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded">Rating: 4.5</span>
+                 ${Array(5)
+                    .fill(
+                      '<svg class="w-5 h-5 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20"><path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/></svg>'
+                    )
+                    .join("")}
             </div>
             <p class="text-lg font-bold text-gray-900">Price: $${clickedProduct.price}</p>
-            <p class="text-gray-700 mt-2">${clickedProduct.description}</p>
+            <p class="text-gray-700 mt-2 line-clamp-2">${clickedProduct.description}</p>
             <p class="text-gray-600 mt-2">Category: ${clickedProduct.category}</p>
             <div class="mt-5">
                 <button id="add-to-cart-btn" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                     Add to cart
                 </button>
             </div>
+    </div>
     </div>`;
 
     let addToCartBtn = document.getElementById("add-to-cart-btn");
@@ -97,3 +105,7 @@ function getDetails(productId) {
   });
   xhr.send();
 }
+// max-w-sm bg-white border border-gray-200 rounded-lg shadow
+// img   p-2 w-24 h-24  mx-auto object-contain rounded-t-lg
+// card  mt-2  w-96 h-[120px] sm:w-72 md:w-80 lg:w-80 rounded-xl shadow-lg overflow-hidden 
+//transition-all duration-300 ease-in-out justify-center
