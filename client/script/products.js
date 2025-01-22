99dd81a9d59aaefef4d36c92d77f2a3306e3781a
Shereen Ahmed <shereenahmed497@gmail.com>
2025-01-22 13:08:47 +0200

    add project view page

--- client/script/products.js
+++ client/script/products.js
@@ -63,6 +63,8 @@ function display() {
         </div>
     </div>
 </div> ` ;
+
+           
     }
 
     // Insert generated HTML into the container
--- client/script/viewProduct.js
+++ client/script/viewProduct.js
@@ -26,31 +26,70 @@ function display(clickedProduct) {
   var boxProduct = document.getElementById("view-product");
 
   if (clickedProduct) {
-    boxProduct.innerHTML = `
-       <div class="flex justify-center items-center">
-        <div class="card mt-20  w-96 h-[450px] sm:w-72 md:w-80 lg:w-80 rounded-xl shadow-lg overflow-hidden 
-        transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl">
-             <img class="p-2 w-60 h-40  mx-auto object-contain rounded-t-lg" src="${clickedProduct.image}" alt="${clickedProduct.title}" />
-             <div class="px-5 pb-5">
-             <h1 class="text-xl font-semibold tracking-tight text-gray-900">${clickedProduct.title}</h1>
-             <div class="flex items-center mt-2.5 mb-5">
-                <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded">Rating: 4.5</span>
+    //boxProduct.innerHTML = `
+    //    <div class="flex justify-center items-center">
+    //     <div class="card mt-20  w-96 h-[450px] sm:w-72 md:w-80 lg:w-80 rounded-xl shadow-lg overflow-hidden 
+    //     transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-2xl">
+    //          <img class="p-2 w-60 h-40  mx-auto object-contain rounded-t-lg" src="${clickedProduct.image}" alt="${clickedProduct.title}" />
+    //          <div class="px-5 pb-5">
+    //          <h1 class="text-xl font-semibold tracking-tight text-gray-900">${clickedProduct.title}</h1>
+    //          <div class="flex items-center mt-2.5 mb-5">
+    //             <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded">Rating: 4.5</span>
+    //              ${Array(5)
+    //                 .fill(
+    //                   '<svg class="w-5 h-5 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20"><path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/></svg>'
+    //                 )
+    //                 .join("")}
+    //         </div>
+    //         <p class="text-lg font-bold text-gray-900">Price: $${clickedProduct.price}</p>
+    //         <p class="text-gray-700 mt-2 line-clamp-2">${clickedProduct.description}</p>
+    //         <p class="text-gray-600 mt-2">Category: ${clickedProduct.category}</p>
+    //         <div class="mt-5">
+    //             <button id="add-to-cart-btn" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+    //                 Add to cart
+    //             </button>
+    //         </div>
+    // </div>
+    // </div>`;
+
+     boxProduct.innerHTML = `
+       <div class="flex justify-center items-center min-h-screen bg-gray-100 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+       <div class="card bg-white rounded-lg shadow-lg p-6 max-w-4xl w-full flex">
+
+           <div class="w-64 flex items-center justify-center">
+              <img 
+                 class="rounded-lg object-cover w-64 max-w-sm"
+                 src="${clickedProduct.image}" alt="${clickedProduct.title}" />
+            </div>
+
+        <div class="w-[400px] pl-10">
+            <div class="flex justify-between items-start">
+            <h1 class="text-xl font-semibold text-gray-900">${clickedProduct.title}</h1>
+            
+               </div>
+            <p class="text-xl font-semibold text-gray-700 mt-2"> $${clickedProduct.price}</p>
+
+            <div class="flex items-center mt-4 mb-5">
+                <div class="flex items-center space-x-1 rtl:space-x-reverse">
                  ${Array(5)
                     .fill(
                       '<svg class="w-5 h-5 text-yellow-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20"><path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/></svg>'
                     )
                     .join("")}
+                    <span class="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">5.0</span>
+                </div>
             </div>
-            <p class="text-lg font-bold text-gray-900">Price: $${clickedProduct.price}</p>
-            <p class="text-gray-700 mt-2 line-clamp-2">${clickedProduct.description}</p>
-            <p class="text-gray-600 mt-2">Category: ${clickedProduct.category}</p>
-            <div class="mt-5">
-                <button id="add-to-cart-btn" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                    Add to cart
-                </button>
-            </div>
-    </div>
-    </div>`;
+
+            <p class="text-gray-600 mt-2">${clickedProduct.description}</p>
+
+
+            <div class="flex justify-between items-center mt-6">
+            <button id="add-to-cart-btn" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                               Add to cart</button>
+             </div>
+        </div>
+      </div>
+  </div>`;
 
     let addToCartBtn = document.getElementById("add-to-cart-btn");
     addToCartBtn.addEventListener("click", () => {
